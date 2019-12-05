566504f567178f3030b208b5b01ddfe403c99f00
MwlLj <731025894@qq.com>
2019-12-05 11:05:54 +0800

    creare

---
+++ cmd/examples/cmd_test/src/cmd_test.nim
@@ -0,0 +1,9 @@
+import "../../../src/cmd"
+
+when isMainModule:
+    var cmdHandler = cmd.newCmd()
+    let root1 = cmdHandler.registerWithDesc("-root1", ".", "search root 1 path")
+    let root2 = cmdHandler.registerWithDesc("-root2", ".", "search root 2 path")
+    cmdHandler.parse()
+    echo(root1[])
+    echo(root2[])
